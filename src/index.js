d83b3a9bf229200bc4a593e5a64cdf02ec147882
amro alwageei  -gt <gt.amro.alwageei@Jt.JtGroup>
2023-09-18 21:20:08 +0300

    first

--- src/index.js
+++ src/index.js
@@ -15,3 +15,92 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// Google Analytics : Performance and user experience of your web application
+// npm install web-vitals
+// yarn add web-vitals
+
+
+
+// React it's Library not Framework  " Javascript Library " 
+// V and C from MVC : Own State and Can sent or pass it to children 
+
+
+// Why Raectjs : Facebook , Instagram , Airbnb , Netflix , Paypal , Dropbox
+
+// 1- Fast to render UI  & Responsive 
+// 2 - Composable Components 
+// 3 - Pluggable 
+
+
+
+// What and Why  , Components & JSX , Props + State , Events , 
+// Outputting Content , Styling , Hooks , Debugging 
+
+
+
+
+// NodeJS -> As a Development Serve - Preview Raect App 
+
+// Local Setup -> locally on ur system / VS Editor  / Require Installed 
+// Browser-based Setup -> via CodeSandbox / No Software installed 
+
+
+// Components ? 
+// 1 - Reusbility ( Don't Repeat yourself )  ,
+// 2 - Separation of Concerns ( Dont do to many things in one and the same place )
+// 3 - Easy to Manage 
+// 4 - Split big chunks of code into multiple smaller function 
+
+
+// ****** Folder & Files *******
+// SRC : 
+
+
+
+// Inside Index.js  : transformation  before it's devlirabel to
+// reportWebVitals();
+// // Google Analytics : Performance and user experience of your web application
+// // npm install web-vitals
+// // yarn add web-vitals
+
+// ****** Folder & Files *******
+
+// ************* 
+// Must React App in Small letter  " because of npm naming restrictions:
+//  * name can no longer contain capital letters  "
+
+
+// Cannot create a project named "react" because a dependency with the same name ex
+// ists.
+// Due to the way npm works, the following names are not allowed:
+//   react
+//   react-dom
+//   react-scripts
+
+// Please choose a different project name.
+// *************
+
+
+// ****** Extenstions *******
+// 1 - Prettier 
+// 2 - ES7+ React/Redux/React-Native snippets
+// React
+// ****** Extenstions *******
+
+
+// ****** Terminal *******
+// npm install
+// npm start
+// cmd
+// ****** Terminal *******
+
+
+// ****** Packages *******
+// 1 - Router 
+// 2 - React Icon
+// 3 - Formik 
+// 4 - Languages 
+// 5 - Bootstrap or Tailwind 
+// 6 - 
+
+// ****** Packages *******
